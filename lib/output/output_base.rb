fd18389c4174876f29c07f0920759516e695b22f
Scott Bellware <sbellware@gmail.com>
2013-01-21 15:52:24 -0600

    Checkpoint (apologies... hacking)

--- lib/output/output_base.rb
+++ lib/output/output_base.rb
@@ -1,9 +1,21 @@
 module Output
   module OutputBase
     def self.included(base)
-      base.send :include, Single
+      # TODO Remove. Single semantics is not related to output semantics
+      # This is entirely a user's choice
+      # base.send :include, Single
+      #
+
+      # TODO Remove. Single semantics is not related to output semantics
+      # This is entirely a user's choice
+      # An Output class must stand entirely on its own
+      # A user of Output should not be made to worry about settings unless
+      # he already knows about settings, and wants them
       base.send :include, Setter::Settings
+      #
+
       base.send :include, WriterCreation
+      # base.extend WriterCreation
     end
 
     def write(method, description)
--- lib/output/writer_creation.rb
+++ lib/output/writer_creation.rb
@@ -3,9 +3,13 @@ module Output
     def self.included(base)
       base.extend ClassMethods
     end
-    def self.extended(base)
-      base.extend ClassMethods
-    end
+
+    # TODO what's this idiom?
+    # What happens when both object and class use this module
+    # ie: can be included AND extended
+    # def self.extended(base)
+    #   base.extend ClassMethods
+    # end
 
     module ClassMethods
       def writers
@@ -13,22 +17,30 @@ module Output
       end
 
       def define_write_method(name, writer_accessor_name, level, transform)
+        # TODO lift to same name
         all_writers = writers
 
         send :define_method, name do |message|
           message = transform.call message
           writer = send writer_accessor_name
+
+          # TODO write method also has responsibility of registering the writer?
+          # No, this logic is purely a concern of creation-time, not run-time
           all_writers << writer unless all_writers.include? writer
+          #
+
           writer.write level, message
           message
         end
+
       end
 
-      def writer(name,options = {}, &transform_block)
+      def writer(name, options = {}, &transform_block)
         transform = transform_block || ->(message) { message } 
-        level = options.fetch(:level,:debug)
-        writer_accessor_name = "#{name}_logger"
+        level = options.fetch(:level, :debug)
+        writer_accessor_name = "#{name}_writer"
 
+        # TODO Optional, only if is a setting (and make sure consts are defined before checking if is a #{const})
         setting writer_accessor_name
 
         define_write_method name, 
