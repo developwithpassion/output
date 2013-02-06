bb0dbeb9e17b7092a2740e1c3743bc30cb9ab383
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-02-05 18:09:26 -0700

    Updated the proofs

--- proofs/suite.rb
+++ proofs/suite.rb
@@ -1,7 +1,3 @@
 require_relative 'proofs_init'
 
-files = Dir.glob("output/**/*.rb").reject do|item| 
-  /_sketch/ =~ item
-end
-
-Proof::Runner::Suite.run files, "demos/**/*.rb"
+Proof::Runner::Suite.run "output/**/*.rb", "demos/**/*.rb"
