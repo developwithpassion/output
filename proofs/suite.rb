31e47ca4d47c4f0c211dc09b8fa878338baaf917
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-02-05 18:11:38 -0700

    Updated the suite script

--- proofs/suite.rb
+++ proofs/suite.rb
@@ -1,3 +1,7 @@
 require_relative 'proofs_init'
 
-Proof::Runner::Suite.run "output/**/*.rb", "demos/**/*.rb"
+files = Dir.glob("output/**/*.rb").reject do |file| 
+  /appender_sketch/ =~ file
+end
+
+Proof::Runner::Suite.run files, "demos/**/*.rb"
