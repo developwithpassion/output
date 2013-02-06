10cacebb1f80cb9613810fbc62df83debc312094
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-02-05 18:22:42 -0700

    Renamed the suite script

--- proofs/suite.rb
+++ proofs/suite.rb
@@ -1,7 +1,3 @@
 require_relative 'proofs_init'
 
-files = Dir.glob("output/**/*.rb").reject do |file| 
-  /appender_sketch/ =~ file
-end
-
-Proof::Runner::Suite.run files, "demos/**/*.rb"
+Proof::Runner::Suite.run "output/**/*.rb", "demos/**/*.rb"
